50cd43cb7846099c472089ae3b99b89048aec019
Artem <artem.mamedov5375@gmail.com>
2024-01-08 12:47:38 +0200

    flowWithLifecycle

--- app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.coroutineflow.crypto_app
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,10 +8,12 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.coroutineflow.crypto_app.CryptoRepository
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
@@ -19,10 +22,18 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
+    val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
-        .map { State.Content(currencyList = it) as State}
-        .onStart { emit(State.Loading) }
-        .asLiveData()
+        .map { State.Content(currencyList = it) as State }
+        .onStart {
+            Log.d("CryptoViewModel", "Started")
+            emit(State.Loading)
+        }
+        .onEach {
+            Log.d("CryptoViewModel", "OnEach")
+        }
+        .onCompletion {
+            Log.d("CryptoViewModel", "Complete")
+        }
 
 }
