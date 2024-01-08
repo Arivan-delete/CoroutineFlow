61c6ce985b6f1358f2bcd358c10e374b6a191c1a
Artem <artem.mamedov5375@gmail.com>
2024-01-08 11:31:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coroutineflow/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.coroutineflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutineflow.crypto_app.CryptoActivity
+import com.example.coroutineflow.databinding.ActivityMainBinding
+import com.example.coroutineflow.lessons.lesson2.UsersActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonUsersActivity.setOnClickListener {
+            startActivity(UsersActivity.newIntent(this))
+        }
+        binding.buttonCryptoActivity.setOnClickListener {
+            startActivity(CryptoActivity.newIntent(this))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.coroutineflow.crypto_app
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.coroutineflow.crypto_app.CryptoRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+class CryptoViewModel : ViewModel() {
+
+    private val repository = CryptoRepository
+
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State}
+        .onStart { emit(State.Loading) }
+        .asLiveData()
+
+}
