64e2e77557adcafd1e04eea1469bc8ac064f136c
Artem <artem.mamedov5375@gmail.com>
2024-01-09 18:21:30 +0200

    Use StateFlow instead of LiveData

--- app/src/main/java/com/example/coroutineflow/MainActivity.kt
+++ app/src/main/java/com/example/coroutineflow/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.coroutineflow.crypto_app.CryptoActivity
 import com.example.coroutineflow.databinding.ActivityMainBinding
 import com.example.coroutineflow.lessons.lesson2.UsersActivity
+import com.example.coroutineflow.team_score.TeamScoreActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         }
         binding.buttonCryptoActivity.setOnClickListener {
             startActivity(CryptoActivity.newIntent(this))
+        }
+        binding.teamScoreActivity.setOnClickListener {
+            startActivity(TeamScoreActivity.newIntent(this))
         }
     }
 }
